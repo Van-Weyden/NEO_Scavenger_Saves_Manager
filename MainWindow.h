c05e2f503ee21047cfcad33a2c28fa492d3c13b8
Van Weyden <slavyanzt@gmail.com>
2021-03-29 23:17:29 +0700

    Version 0.9

---
+++ MainWindow.h
@@ -0,0 +1,90 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+class QSettings;
+class QTranslator;
+
+class SavesModel;
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+static constexpr int applicationVersion(const int major, const int minor = 0, const int micro = 0)
+{
+	return ((major << 20) | (minor << 10) | micro);
+}
+
+inline constexpr int majorApplicationVersion(int version)
+{
+	return (version >> 20);
+}
+
+inline constexpr int minorApplicationVersion(int version)
+{
+	return ((version  & 1047552) >> 10);
+}
+
+inline constexpr int microApplicationVersion(int version)
+{
+	return (version & 1023);
+}
+
+QString applicationVersionToString(const int version);
+int applicationVersionFromString(const QString &version);
+
+class MainWindow : public QMainWindow
+{
+	Q_OBJECT
+
+public:
+	static constexpr const int CurrentApplicationVersion = applicationVersion(1, 0, 0);
+	static constexpr const char *AutosavePrefix = "autosave_";
+	static constexpr const char *QuicksavePrefix = "quicksave_";
+	static constexpr const char *GameSaveFileName = "nsSGv1.sol";
+
+
+	MainWindow(QWidget *parent = nullptr);
+	~MainWindow();
+
+	bool backupSave(const QString &backupSaveName);
+	void restoreSave(const QString &backupSaveName);
+	bool setLanguage(const QString &lang);
+
+public slots:
+	void backupQuickSave();
+	void restoreLastQuickSave();
+	bool backupCurrentSave();
+	void restoreSelectedSave();
+	void deleteSelectedSave();
+	void setOriginSaveCheckInterval(const int interval);
+
+protected:
+	void timerEvent(QTimerEvent *event) override;
+
+private slots:
+	void onCurrentRowChanged(const QModelIndex &currentRowIndex);
+
+private:
+	void checkOriginSave();
+	void scanSaves();
+	void readSettings();
+	void saveSettings() const;
+	Ui::MainWindow *ui;
+
+	QSettings *m_settings = nullptr;
+	QTranslator *m_qtTranslator = nullptr;
+	QTranslator *m_translator = nullptr;
+	QString m_lang = "";
+
+	int m_timerId = 0;
+
+	QByteArray m_originSaveLastChecksum;
+	int m_lastAutosaveIndex = -1;
+	int m_lastQuicksaveIndex = -1;
+
+	SavesModel *m_model = nullptr;
+};
+#endif // MAINWINDOW_H
---
+++ SavesModel.h
@@ -0,0 +1,73 @@
+#ifndef SAVESMODEL_H
+#define SAVESMODEL_H
+
+#include <QAbstractTableModel>
+
+class QFileInfo;
+typedef QList<QFileInfo> QFileInfoList;
+
+class SavesModel : public QAbstractTableModel
+{
+public:
+	static constexpr const char *SavesExtension = ".sol";
+
+	SavesModel(QObject *parent = nullptr);
+	virtual ~SavesModel() = default;
+
+	void clear();
+	bool contains(const QString &name) const; //inline
+	int indexOf(const QString &name) const;
+	void fill(const QFileInfoList &fileInfoList);
+	bool insertRow(int row, const QFileInfo &fileInfo);
+
+	Qt::ItemFlags flags(const QModelIndex &index) const override;
+
+	inline int rowCount(const QModelIndex &parent = QModelIndex()) const override;
+	inline int columnCount(const QModelIndex &parent = QModelIndex()) const override;
+	QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+	bool setData(const QModelIndex &index, const QVariant &value, int role = Qt::EditRole) override;
+	QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const override;
+
+	bool insertRows(int row, int count, const QModelIndex &parent = QModelIndex()) override;
+	bool removeRows(int row, int count, const QModelIndex &parent = QModelIndex()) override;
+	bool moveRows(const QModelIndex &sourceParent, int sourceRow, int count,
+							  const QModelIndex &destinationParent, int destinationChild) override;
+
+
+private:
+	struct SaveInfo
+	{
+		SaveInfo(const QFileInfo &fileInfo);
+		void rename(const QString name);
+		void setLastModified(const QDateTime &dateTime);
+
+		QString name;
+		QString lastModified;
+		QString path;
+
+	};
+
+	QList<SaveInfo> m_savesInfo;
+};
+
+
+
+
+inline bool SavesModel::contains(const QString &name) const
+{
+	return (indexOf(name) != -1);
+}
+
+inline int SavesModel::rowCount(const QModelIndex &parent) const
+{
+	Q_UNUSED(parent);
+	return m_savesInfo.count();
+}
+
+inline int SavesModel::columnCount(const QModelIndex &parent) const
+{
+	Q_UNUSED(parent);
+	return 2;
+}
+
+#endif // SAVESMODEL_H
